4f9b5534d5c28fa0fbff8133dcaefc0e29f21f7c
maoxin53 <maoxin53@gmail.com>
2021-05-25 00:06:37 +0800

    <a> removed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,16 +6,14 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand">
-          <h2 style={{ color: "whitesmoke" }}>
-            Present<strong style={{ color: "GoldenRod" }}>AI</strong>ble
-          </h2>
-          <Link to="/">Main{"  "}</Link>
-          <Link to="/about">About us{"  "}</Link>
-          <Link to="/dashboard">Dashboard{"  "}</Link>
-          <Link to="/signup">Sign Up{"  "}</Link>
-          <span className="badge badge-pill bg-warning">{totalCounters}</span>
-        </a>
+        <h2 style={{ color: "whitesmoke" }}>
+          Present<strong style={{ color: "GoldenRod" }}>AI</strong>ble
+        </h2>
+        <Link to="/">Main{"  "}</Link>
+        <Link to="/about">About us{"  "}</Link>
+        <Link to="/dashboard">Dashboard{"  "}</Link>
+        <Link to="/signup">Sign Up{"  "}</Link>
+        <span className="badge badge-pill bg-warning">{totalCounters}</span>
       </div>
     </nav>
   );
